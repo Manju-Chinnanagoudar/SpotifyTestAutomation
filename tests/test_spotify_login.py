35dc4b847dae1930595a80ca6d1c721dcb51143e
Manjunath <mschinnanagoudar@gmail.com>
2023-05-13 16:48:35 +0530

    Valid login test case

---
+++ tests/test_spotify_login.py
@@ -0,0 +1,38 @@
+import pytest
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+from pages.login_page import LogInPage
+from pages.common.header_section import HeaderSection
+
+@pytest.fixture()
+def browser():
+    driver =  webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+def test_login_valid(browser):
+    # Initialize the pages
+    login_page = LogInPage(browser)
+    header = HeaderSection(browser)
+
+    # Navigate to the spotify
+    login_page.navigate(login_page.config.get('APP', 'url'))
+
+    # Click on the login button
+    header.click_login_btn()
+
+    # Login with valid credentials 
+    is_logged_in = login_page.login_with_email(
+        login_page.config.get('CREDENTIALS', 'username'),
+        login_page.config.get('CREDENTIALS', 'password'))
+    
+    # Verify successful login
+    assert is_logged_in == True
+    
+    # Validate the user name 
+    assert header.is_logged_in() == True
